0243c0fb8b947b2986f47cd9cbfef8c884a985bf
Uli <sorjak1@gmail.com>
2025-12-26 22:16:25 -0800

    updating ansible to use root the whole way

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-ANSIBLE_USER=uli
+NEW_USER="uli"
+PUBKEY_FILE="/home/uli/.ssh/id_ed25519.pub"
 
 HOST_IP="$1"
 if [ -z $HOST_IP ]; then
@@ -9,6 +10,13 @@ if [ -z $HOST_IP ]; then
   exit 1
 fi
 
+USER_PASSWORD="$2"
+if [ -z $USER_PASSWORD ]; then
+  echo "The password argument missing or empty"
+  exit 1
+fi
+
+echo "Running ansible playbook on host $HOST_IP with user $NEW_USER and password $USER_PASSWORD"
 pushd ansible
-ansible-playbook playbook.yml -i $HOST_IP, -e ansible_user=$ANSIBLE_USER --ask-become-pass
+ansible-playbook playbook.yml -k -i $HOST_IP, -e ansible_user=root -e new_user=$NEW_USER -e user_pw=$USER_PASSWORD -e pubkey_file=$PUBKEY_FILE
 popd
